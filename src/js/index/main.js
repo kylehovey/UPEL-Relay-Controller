df95f7cd1636128224f560ba7ced4fbf14304d1d
kylehovey <khoveyc@gmail.com>
2017-08-10 10:18:29 -0600

    Used toggle group instead of wheelnav.

--- src/js/index/main.js
+++ src/js/index/main.js
@@ -5,7 +5,9 @@ $(() => {
   // Set listeners
   $("#active-relay-select").on('click', e => {
     // Set the relay
-    let i = app.activeRelay = $(e.target).data("value");
+    let i = app.activeRelay = $(e.target)
+      .find("input")
+      .data("value");
 
     // Set button text accordingly
     app.bigButton.setText(app.buttonStates[i] ? `Deactivate Relay ${i + 1}` : `Activate Relay ${i + 1}`);
