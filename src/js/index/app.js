8bb44ed4fc9b55b7e2a8b81e301fa500fcf264b8
kylehovey <khoveyc@gmail.com>
2017-08-10 09:55:38 -0600

    Cleaned up code a bit.

--- src/js/index/app.js
+++ src/js/index/app.js
@@ -2,63 +2,61 @@ class App {
   constructor() {
     // Init active relay control
     this.activeRelay = 0;
+  }
 
-    //wheel.navItems.forEach((item, i) => {
-    //  item.navigateFunction = () => {
-    //    // Set the relay
-    //    this.activeRelay = i;
-
-    //    // Set button text accordingly
-    //    this.bigButton.setText(this.buttonStates[i] ? `Deactivate Relay ${i + 1}` : `Activate Relay ${i + 1}`);
-    //  }
-    //});
+  /**
+   * Run an API command
+   * @param {String} command Command to execute
+   * @param {Object} data Data to send
+   * @return {Promise}
+   */
+  async runCommand(command, data) {
+    return (await $.post({
+      url : "/api",
+      data : {
+        command,
+        data : JSON.stringify(data)
+      }
+    })).results;
   }
 
   /**
    * Initialize application
    */
   async init() {
-    /**
-     * Run an API command
-     * @param {String} command Command to execute
-     * @param {Object} data Data to send
-     * @return {Promise}
-     */
-    async function runCommand(command, data) {
-      return (await $.post({
-        url : "/api",
-        data : {
-          command,
-          data : JSON.stringify(data)
-        }
-      })).results;
-    }
 
     // Get the current state of each relay
-    this.buttonStates = await runCommand("getStatus");
+    this.buttonStates = await this.runCommand("getStatus");
 
     // Create the main button
     this.bigButton = new BigButton({
       containerID : "main-button",
       name : "Main Button",
-      text : this.buttonStates[this.activeRelay] ? `Deactivate Relay ${this.activeRelay + 1}` : `Activate Relay ${this.activeRelay + 1}`,
+      text : this.buttonStates[this.activeRelay] ?
+        `Deactivate Relay ${this.activeRelay + 1}` :
+        `Activate Relay ${this.activeRelay + 1}`,
       onClick : () => {
-        // Fencepost name for relay
-        let relayNo = this.activeRelay + 1;
-
         // Invert button state
-        let state = this.buttonStates[this.activeRelay] = !this.buttonStates[this.activeRelay];
-
-        // Set button text accordingly
-        app.bigButton.setText(state ? `Deactivate Relay ${relayNo}` : `Activate Relay ${relayNo}`);
+        let state
+          = this.buttonStates[this.activeRelay]
+          = !this.buttonStates[this.activeRelay];
 
         // Run the command
-        runCommand("set", {
+        this.runCommand("set", {
           number : this.activeRelay,
           state
         })
           .then(console.log)
           .catch(console.log)
+
+        // Fencepost name for relay
+        let relayNo = this.activeRelay + 1;
+
+        // Set button text accordingly
+        app.bigButton.setText(state ?
+          `Deactivate Relay ${relayNo}` :
+          `Activate Relay ${relayNo}`
+        );
       }
     });
   }
